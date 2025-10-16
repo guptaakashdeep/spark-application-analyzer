99bf0c862fdc4344e8f0bee4fe92953319d84206
Akashdeep Gupta <guptaakashdeep@users.noreply.github.com>
2025-10-16 17:37:56 +0530

    Integration of Bottleneck identification

--- spark_application_analyzer/analyzer_service.py
+++ spark_application_analyzer/analyzer_service.py
@@ -1,11 +1,14 @@
 from datetime import datetime
 from typing import Optional
+
+from spark_application_analyzer.analytics.base import BaseExecutorStrategy
+from spark_application_analyzer.analytics.base import BaseMemoryStrategy
+from spark_application_analyzer.analytics.base import BasePerformanceStrategy
 from spark_application_analyzer.collectors.datasource import IDataSource
+from spark_application_analyzer.models.executor_metrics import SparkApplication
+from spark_application_analyzer.models.recommendation import AnalysisResults
+from spark_application_analyzer.models.recommendation import Bottlenecks
 from spark_application_analyzer.models.recommendation import Recommendation
-from spark_application_analyzer.analytics.base import (
-    BaseMemoryStrategy,
-    BaseExecutorStrategy,
-)
 from spark_application_analyzer.utils.cli_colors import Colors
 
 
@@ -17,19 +20,27 @@ class AnalyzerService:
         datasource: IDataSource,
         memory_strategy: BaseMemoryStrategy,
         executor_strategy: BaseExecutorStrategy,
+        performance_strategy: BasePerformanceStrategy,
     ):
         self.datasource = datasource
         self.memory_strategy = memory_strategy
         self.executor_strategy = executor_strategy
+        self.performance_strategy = performance_strategy
 
     def generate_recommendations(
-        self, app_id: str, attempt_id: Optional[str], emr_id: Optional[str]
-    ) -> Recommendation:
+        self,
+        app_details: SparkApplication,
+        attempt_id: Optional[str],
+        emr_id: Optional[str],
+    ) -> AnalysisResults:
         """
-        Fetches data, runs analysis, and returns a recommendation object.
+        Fetches data, runs analysis, and returns a analysis results object containing both recommendation and bottlenecks.
         """
         # 1. Get application environment data
-        app_details = self.datasource.get_application(app_id)
+        # app_details = self.datasource.get_application(app_id)
+        app_id = app_details.id
+        # TODO: attempts object should be created from app_details filterd with attempt_id
+        # this attemptId object should be used for getting further details.
         app_env = self.datasource.get_environment(app_id, attempt_id)
 
         # 2. Perform validation
@@ -81,12 +92,29 @@ class AnalyzerService:
         # 4. Assemble the final Recommendation object
         recommendation = Recommendation(
             application_id=app_id,
-            app_name=app_details["name"],
+            app_name=app_details.name,
+            time_taken_mins=round((app_details.attempts[0]["duration"] / 1000) / 60, 2),
             metrics_collection_dt=datetime.now().date(),
             emr_id=emr_id,
-            additional_details=defined_exec_params,
+            current_configuration=defined_exec_params,
             **mem_recommendations,
             **exec_num_recommendations,
         )
 
-        return recommendation
+        # Bottleneck identification
+        job_details = self.datasource.get_job_metrics(app_id, attempt_id)
+        num_jobs = len(job_details)
+        stage_details = self.datasource.get_stage_metrics(app_id, attempt_id)
+        num_stages = len(stage_details)
+        bottlenecks = Bottlenecks(
+            **self.performance_strategy.identify_bottlenecks(
+                stage_details, job_details, executor_details
+            ),
+            **{"num_jobs": num_jobs, "num_stages": num_stages},
+        )
+
+        analysis_results = AnalysisResults(
+            recommendation=recommendation, bottlenecks=bottlenecks
+        )
+
+        return analysis_results
--- spark_application_analyzer/api.py
+++ spark_application_analyzer/api.py
@@ -1,10 +1,15 @@
 from typing import Optional
-from spark_application_analyzer.collectors.spark_history import SparkHistoryServerClient
-from spark_application_analyzer.analyzer_service import AnalyzerService
-from spark_application_analyzer.analytics.memory import MaxMemoryStrategy
+
+from spark_application_analyzer.analytics.bottleneck import (
+    PerformanceBottleneckStrategy,
+)
 from spark_application_analyzer.analytics.executors import IdleTimeStrategy
+from spark_application_analyzer.analytics.memory import MaxMemoryStrategy
+from spark_application_analyzer.analyzer_service import AnalyzerService
+from spark_application_analyzer.collectors.spark_history import SparkHistoryServerClient
+from spark_application_analyzer.models.executor_metrics import SparkApplication
+from spark_application_analyzer.models.recommendation import AnalysisResults
 from spark_application_analyzer.storage.arrow_io import ParquetSink
-from spark_application_analyzer.models.recommendation import Recommendation
 from spark_application_analyzer.utils.aws import get_history_server_url
 
 
@@ -13,7 +18,7 @@ def analyze_application(
     emr_id: Optional[str] = None,
     base_url: Optional[str] = None,
     sink_path: Optional[str] = None,
-) -> Recommendation:
+) -> AnalysisResults:
     """
     A high-level function to analyze a Spark application and generate recommendations.
 
@@ -22,9 +27,9 @@ def analyze_application(
 
     :param application_id: The ID of the Spark application to analyze.
     :param emr_id: Optional EMR cluster ID. If provided, the Spark History Server URL
-                   will be discovered automatically.
+                    will be discovered automatically.
     :param base_url: The base URL of the Spark History Server. Used if emr_id is not provided.
-                     Defaults to http://localhost:18080 if neither is provided.
+                    Defaults to http://localhost:18080 if neither is provided.
     :param sink_path: Optional path to save the recommendation parquet file.
     :return: A Recommendation object containing the analysis results.
     """
@@ -41,22 +46,32 @@ def analyze_application(
     # 2. Instantiate the default strategies
     memory_strategy = MaxMemoryStrategy()
     executor_strategy = IdleTimeStrategy()
+    performance_strategy = PerformanceBottleneckStrategy()
 
     # 3. Initialize the Analyzer Service
-    analyzer = AnalyzerService(source, memory_strategy, executor_strategy)
+    analyzer = AnalyzerService(
+        source, memory_strategy, executor_strategy, performance_strategy
+    )
 
     # 4. Get application details to find the latest attempt
-    app_details = analyzer.datasource.get_application(application_id)
-    attempt_id = app_details["attempts"][0].get("attemptId")
+    app_details = SparkApplication(
+        **analyzer.datasource.get_application(application_id)
+    )
+    # TODO: what happens if there are multiple attempts ?!?!
+    # Handle this based on getting the one with completed status.
+    attempt_id = app_details.attempts[0].get("attemptId")
+    # attempt_id = app_details["attempts"][0].get("attemptId")
 
     # 5. Generate recommendations
-    recommendation = analyzer.generate_recommendations(
-        app_id=application_id, attempt_id=attempt_id, emr_id=emr_id
+    results = analyzer.generate_recommendations(
+        app_details=app_details, attempt_id=attempt_id, emr_id=emr_id
     )
+    # recommendation = results.recommendation
+    # bottlenecks = results.bottlenecks
 
     # 6. Optionally save the results
     if sink_path:
         sink = ParquetSink(sink_path)
-        sink.save(recommendation)
+        sink.save(results)
 
-    return recommendation
+    return results
