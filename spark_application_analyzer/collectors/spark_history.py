4cbcd8dd7fc803d747776e1925872b3c2d14bd30
Akashdeep Gupta <guptaakashdeep@users.noreply.github.com>
2025-10-16 17:23:30 +0530

    Changes in Spark Metrics collector to get stage and task details

--- spark_application_analyzer/collectors/spark_history.py
+++ spark_application_analyzer/collectors/spark_history.py
@@ -1,17 +1,19 @@
 """Spark History Server client for collecting application and executor metrics."""
-
 import logging
-import requests
-from typing import List, Optional, Dict, Any
-from dataclasses import asdict
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Optional
 
-from spark_application_analyzer.models.executor_metrics import (
-    SparkApplication,
-    ApplicationAttempt,
-    ExecutorMetrics,
-    PeakExecutorMetrics,
-)
+import requests
+from spark_application_analyzer.models.executor_metrics import ApplicationAttempt
+from spark_application_analyzer.models.executor_metrics import ExecutorMetrics
+from spark_application_analyzer.models.executor_metrics import PeakExecutorMetrics
+from spark_application_analyzer.models.executor_metrics import SparkApplication
+from spark_application_analyzer.models.job_metrics import JobMetrics
+from spark_application_analyzer.models.stage_metrics import StageMetrics
 from spark_application_analyzer.utils.conversions import convert_dt_to_ms
+
 from .datasource import IDataSource
 
 logger = logging.getLogger(__name__)
@@ -53,6 +55,22 @@ class SparkHistoryServerClient(IDataSource):
         )
         return self._get(executors_url)
 
+    def _get_all_stages(self, app_id: str, attempt_id: Optional[int]) -> List[dict]:
+        all_stages_url = (
+            f"/applications/{app_id}/{attempt_id}/stages"
+            if attempt_id
+            else f"/applications/{app_id}/stages"
+        )
+        return self._get(all_stages_url)
+
+    def _get_all_jobs(self, app_id: str, attempt_id: Optional[int]) -> List[dict]:
+        all_jobs_url = (
+            f"/applications/{app_id}/{attempt_id}/jobs"
+            if attempt_id
+            else f"/applications/{app_id}/jobs"
+        )
+        return self._get(all_jobs_url)
+
     def get_peak_memory_metrics(
         self, app_id: str, attempt_id: Optional[int]
     ) -> List[Dict[str, Any]]:
@@ -80,9 +98,11 @@ class SparkHistoryServerClient(IDataSource):
         app_attempt = application_detail.attempts[0]
         # To handle cases where application even after completion is also not moved to
         # Completed applications
-        application_end_time = (app_attempt.end_time_epoch 
-                    if app_attempt.completed else app_attempt.last_updated_epoch
-                    )
+        application_end_time = (
+            app_attempt.end_time_epoch
+            if app_attempt.completed
+            else app_attempt.last_updated_epoch
+        )
         executor_details = self.get_executors(app_id, attempt_id)
         for executor in executor_details:
             if executor["id"] == "driver" or "peakMemoryMetrics" not in executor.keys():
@@ -93,14 +113,50 @@ class SparkHistoryServerClient(IDataSource):
                     host=executor.get("host"),
                     is_active=executor.get("isActive"),
                     total_duration=executor.get("totalDuration"),
+                    total_gc_time=executor.get("totalGCTime"),
                     total_cores=executor.get("totalCores"),
                     max_tasks=executor.get("maxTasks"),
                     max_memory=executor.get("maxMemory"),
                     add_time=convert_dt_to_ms(executor.get("addTime")),
-                    remove_time=convert_dt_to_ms(remove_time_str) if (remove_time_str := executor.get("removeTime")) else application_end_time,
+                    remove_time=convert_dt_to_ms(remove_time_str)
+                    if (remove_time_str := executor.get("removeTime"))
+                    else application_end_time,
                     peak_executor_metrics=PeakExecutorMetrics(
                         **executor["peakMemoryMetrics"]
                     ),
                 )
             )
         return executor_metrics
+
+    def get_stage_metrics(
+        self, app_id: str, attempt_id: Optional[int]
+    ) -> List[StageMetrics]:
+        """Get metrics for all the stages in an application"""
+        all_stages = self._get_all_stages(app_id, attempt_id)
+        stages = [
+            StageMetrics(
+                stage_id=stage["stageId"],
+                status=stage["status"],
+                memory_bytes_spilled=stage["memoryBytesSpilled"],
+                disk_bytes_spilled=stage["diskBytesSpilled"],
+                name=stage["name"],
+                num_tasks=stage["numTasks"],
+                num_failed_tasks=stage["numFailedTasks"],
+                num_killed_tasks=stage["numKilledTasks"],
+                first_task_launched_time=stage["firstTaskLaunchedTime"],
+                completion_time=stage.get("completionTime"),
+            )
+            for stage in all_stages
+            if stage["status"] not in ["RUNNING", "SKIPPED"]
+        ]
+        return stages
+
+    def get_job_metrics(
+        self, app_id: str, attempt_id: Optional[int]
+    ) -> List[JobMetrics]:
+        """Get metrics for all the jobs in an application"""
+        all_jobs = self._get_all_jobs(app_id, attempt_id)
+        jobs = [
+            JobMetrics(**job) for job in all_jobs if job["status"] not in ["RUNNING"]
+        ]
+        return jobs
